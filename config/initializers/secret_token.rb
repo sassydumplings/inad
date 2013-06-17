291a78bdb9ca0f50dbad2946ff10623ce0558e76
Patsy Price <patsy.m.price@gmail.com>
2013-06-17 10:08:20 -0700

    Inital Rails Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inad::Application.config.secret_token = '63b464c054fbc5afa02fbb7a655dd2702810512f623fc4b4c4cb7b8ebf210a88dc29c5b88df99940c1ed9d0a71533f6eba0b9c5ba1665b5513daf0a4888a87b1'
